a297fcd882c0f96d541b6a4d5b95bdce6a8e3f11
Natalia Fedorova <nataliafedorova1989@gmail.com>
2021-08-05 15:06:08 +0200

    a simplified version of the list, still work in progress

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,22 +1,32 @@
 import {useState} from "react";
 
-export default function MyForm({ onSubmit }){
+export default function Form({ onSubmit }){
     const [name, setName] = useState("");
     const [price, setPrice] =useState("");
+    const [isDone, setIsDone] =useState(false);
 
     const handleSubmit = () => {
         onSubmit({
             name: name,
-            price: price
+            price: price,
+            isDone: isDone
         })
 
         setName("");
         setPrice("");
+        setIsDone(false);
+    }
+
+    const validateForm = () => {
+        const errors = {};
+        if (!name) errors.name = "You must write the item name";
+        if (!price) errors.price = "You must write the item price";
+        return errors;
     }
 
     return(
         <form>
-            <p>Add a new shopping item:</p>
+            <h3>Add a new shopping item:</h3>
             <input type="text" placeholder="Item Name" value={name} onChange={(e) => setName(e.target.value)}/>
             <input type="number" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)}/>
             <button type="button" onClick={handleSubmit}>Add</button>
--- src/ShoppingItem.jsx
+++ src/ShoppingItem.jsx
@@ -1,8 +1,8 @@
-export default function ShoppingItem({item}){
+export default function ShoppingItem({item, index, markDone}){
     return(
         <div className="items">
             <div>
-                <input type="checkbox"/>
+                <input type="checkbox" onClick={() => markDone(index)}/>
             </div>
             <p>{item.name}</p> 
             <p>{item.price}:-</p>
